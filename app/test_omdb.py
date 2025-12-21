f6610e5439e68e0145aa6318d0b141a6dfc37bb5
Anastasiia Ivanova <aivanova@zit.local>
2025-12-21 18:53:51 +0100

    Change folder

--- app/test_omdb.py
+++
@@ -1,4 +0,0 @@
-from app.providers.omdb import film_von_omdb_holen
-
-film = film_von_omdb_holen("Dune", 2021)
-print(film)
--- app/test_repo.py
+++
@@ -1,9 +0,0 @@
-from app.repository import FilmeRepository
-from app.models import FilmeStatus
-
-repo = FilmeRepository()
-film = repo.hinzu("Dune", 2021, ["Sci-Fi"])
-repo.hinzufuegen_kommentar(film.id, "Sehr gut")
-repo.set_status(film.id, FilmeStatus.GESEHEN)
-
-print(repo.list_alle())
